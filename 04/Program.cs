2628b848290fd6b2bde20a72e6448dbcfdc0f2d4
Oleg <olegashepelev@gmail.com>
2023-01-29 20:46:52 +0600

    Complete fourth task

--- 04/Program.cs
+++ 04/Program.cs
@@ -4,9 +4,14 @@
 // наименьший элемент массива.
 
 Console.Clear();
-int[,] array = GetArray(6, 8, 0, 10);
+int[,] array = GetArray(5, 5, 0, 10);
 PrintArray(array);
-PrintArray(array);
+int[,] minEl = new int[1, 2];
+minEl = FindMinElement(array, minEl);
+
+Console.WriteLine("Удаление строки и столбца при пересечении наименьшим значении в массиве:");
+int[,] newArray = DeleteRowColumn(array, minEl);
+PrintArray(newArray);
 
 int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
@@ -15,7 +20,7 @@ int[,] GetArray(int m, int n, int minValue, int maxValue)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i, j] = new Random().Next(minValue, maxValue + 1);
+            result[i, j] = new Random().Next(minValue, maxValue);
         }
     }
     return result;
@@ -33,16 +38,43 @@ void PrintArray(int[,] inArray)
     }
 }
 
-int[,] DeleteRowColumn(int[,] array)
+int[,] FindMinElement(int[,] array, int[,] position)
+{
+    int temp = array[0, 0];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] <= temp)
+            {
+                position[0, 0] = i;
+                position[0, 1] = j;
+                temp = array[i, j];
+            }
+        }
+    }
+    return position;
+}
+
+int[,] DeleteRowColumn(int[,] array, int[,] positionMinEl)
 {
     int[,] newArray = new int[array.GetLength(0) - 1, array.GetLength(1) - 1];
-    int minNumber = array[0, 0];
-    for (int i = 0; i < length; i++)
+    int k = 0, l = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < length; j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-
+            if (positionMinEl[0, 0] != i && positionMinEl[0, 1] != j)
+            {
+                newArray[k, l] = array[i, j];
+                l++;
+            }
         }
-
+        l = 0;
+        if (positionMinEl[0, 0] != i)
+        {
+            k++;
+        }
     }
+    return newArray;
 }
